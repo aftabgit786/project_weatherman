e09916c5f2eab417639d37129568bc99d496478d
aftabgit786 <gb485585@gmail.com>
2023-06-07 12:27:09 +0500

    update

--- task_3.py
+++ task_3.py
@@ -9,6 +9,7 @@ for file_value in file_values:
     sliced_date = date.split('-')[MappingIndex.sliced_date]
     maximum_temperature = file_value[MappingIndex.maximum_temperature]
     minimum_temperature = file_value[MappingIndex.minimum_temperature]
+
     if maximum_temperature and minimum_temperature is not None:
         print(f"{sliced_date}{int(maximum_temperature) * '+'} {maximum_temperature}C")
         print(f"{sliced_date}{int(minimum_temperature) * '+'} {minimum_temperature}C")
