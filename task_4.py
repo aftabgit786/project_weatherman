814c49795a027d8e581869a3c7ae90c9f58fb3b1
aftabgit786 <gb485585@gmail.com>
2023-06-07 18:35:25 +0500

    changes apply

--- task_4.py
+++ task_4.py
@@ -5,6 +5,10 @@ from constant import MappingIndex
 month = input('Enter month: ')
 file_values = read_files(month)
 
+red_code = '\033[91m'
+blue_code = '\033[34m'
+reset_code = '\033[0m'
+
 for file_value in file_values:
     date = file_value[MappingIndex.date]
     sliced_date = date.split('-')[MappingIndex.sliced_date]
@@ -12,5 +16,5 @@ for file_value in file_values:
     minimum_temperature = file_value[MappingIndex.minimum_temperature]
 
     if maximum_temperature and minimum_temperature is not None:
-        print(f"{sliced_date}{int(minimum_temperature) * '+'}{int(maximum_temperature) * '+'}"
-              f" {minimum_temperature}C - {maximum_temperature}C")
+        print(f"{sliced_date}{blue_code}{int(minimum_temperature) * '+'}{red_code}{int(maximum_temperature) * '+'}"
+              f"{reset_code} {minimum_temperature}C - {maximum_temperature}C")
