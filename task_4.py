f17a0da0b18d08a7c5bc2545dd5d89d1a3c27e7d
aftabgit786 <gb485585@gmail.com>
2023-06-08 10:55:18 +0500

    final code

--- task_4.py
+++ task_4.py
@@ -2,19 +2,24 @@ from utils import read_files
 from constant import MappingIndex
 
 
-month = input('Enter month: ')
-file_values = read_files(month)
-
 red_code = '\033[91m'
 blue_code = '\033[34m'
 reset_code = '\033[0m'
 
-for file_value in file_values:
-    date = file_value[MappingIndex.date]
-    sliced_date = date.split('-')[MappingIndex.sliced_date]
-    maximum_temperature = file_value[MappingIndex.maximum_temperature]
-    minimum_temperature = file_value[MappingIndex.minimum_temperature]
+num_reports = int(input('Enter the number of reports: '))
 
-    if maximum_temperature and minimum_temperature is not None:
-        print(f"{sliced_date}{blue_code}{int(minimum_temperature) * '+'}{red_code}{int(maximum_temperature) * '+'}"
-              f"{reset_code} {minimum_temperature}C - {maximum_temperature}C")
+for number in range(num_reports):
+    month = input('Enter month: ')
+    file_values = read_files(month)
+
+    print(f"Report for {month}:")
+
+    for file_value in file_values:
+        date = file_value[MappingIndex.date]
+        sliced_date = date.split('-')[MappingIndex.sliced_date]
+        maximum_temperature = file_value[MappingIndex.maximum_temperature]
+        minimum_temperature = file_value[MappingIndex.minimum_temperature]
+
+        if maximum_temperature and minimum_temperature is not None:
+            print(f"{sliced_date}{red_code}{int(maximum_temperature) * '+'}{reset_code} {maximum_temperature}C")
+            print(f"{sliced_date}{blue_code}{int(minimum_temperature) * '+'}{reset_code} {minimum_temperature}C")
